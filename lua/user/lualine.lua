7c08f40b261952dccbce1d0c28132215f083c351
royyuewang <royyuewang@tencent.com>
2023-10-26 15:00:22 +0800

    update lualine config

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -16,7 +16,7 @@ function M.config()
 
   local diagnostics = {
     "diagnostics",
-    sources = { "nvim_diagnostic" },
+    sources = { "nvim_lsp" },
     sections = { "error", "warn" },
     symbols = { error = " ", warn = " " },
     colored = false,
@@ -40,9 +40,38 @@ function M.config()
     padding = 0,
   }
 
+  local filename = {
+    'filename',
+    file_status = true,     -- Displays file status (readonly status, modified status)
+    newfile_status = false, -- Display new file status (new file means no write after created)
+    path = 1,
+    -- 0: Just the filename
+    -- 1: Relative path
+    -- 2: Absolute path
+    -- 3: Absolute path, with tilde as the home directory
+    -- 4: Filename and parent dir, with tilde as the home directory
+
+    shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+    -- for other components. (terrible name, any suggestions?)
+    symbols = {
+      modified = '[+]',      -- Text to show when the file is modified.
+      readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
+      unnamed = '[No Name]', -- Text to show for unnamed buffers.
+      newfile = '[New]',     -- Text to show for newly created file before first write
+    }
+
+  }
+
+  local searchcount = {
+    'searchcount',
+    maxcount = 999,
+    timeout = 500,
+  }
+
   local spaces = function()
     return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
   end
+
   lualine.setup {
     options = {
       globalstatus = true,
@@ -55,8 +84,9 @@ function M.config()
     },
     sections = {
       lualine_a = { "mode" },
-      lualine_b = { "branch" },
-      lualine_c = { diagnostics },
+      lualine_b = { "branch", searchcount },
+      lualine_c = { filename },
+
       lualine_x = { diff, spaces, "encoding", filetype },
       lualine_y = { location },
       lualine_z = { "progress" },
